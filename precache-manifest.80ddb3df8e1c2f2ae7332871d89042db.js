e7301e454976d8ea1fad3d547839bff9cc1abc94
Oleh <olegbudurovych@gmail.com>
2019-02-11 22:58:35 +0200

    Updates

---
+++ precache-manifest.80ddb3df8e1c2f2ae7332871d89042db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a314a311352c0c28d69e",
+    "url": "/robofriends-redux/static/js/runtime~main.a314a311.js"
+  },
+  {
+    "revision": "105f513a1481478f7a58",
+    "url": "/robofriends-redux/static/js/main.105f513a.chunk.js"
+  },
+  {
+    "revision": "c50fd264c2c7f6f64c4e",
+    "url": "/robofriends-redux/static/js/2.c50fd264.chunk.js"
+  },
+  {
+    "revision": "105f513a1481478f7a58",
+    "url": "/robofriends-redux/static/css/main.2206ff37.chunk.css"
+  },
+  {
+    "revision": "c50fd264c2c7f6f64c4e",
+    "url": "/robofriends-redux/static/css/2.4e3f6a17.chunk.css"
+  },
+  {
+    "revision": "bc6413837c3cdcd77369d1b18dfdb2b3",
+    "url": "/robofriends-redux/index.html"
+  }
+];
